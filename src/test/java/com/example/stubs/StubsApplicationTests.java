4e9995c20ae18b9b2f9912cbc6ab782c7f7a6ed2
Esmeregon <39825340+Esmeregon@users.noreply.github.com>
2021-07-04 19:38:30 +0300

    adding new stubs

--- src/test/java/com/example/stubs/StubsApplicationTests.java
+++ src/test/java/com/example/stubs/StubsApplicationTests.java
@@ -14,15 +14,18 @@ import static io.restassured.RestAssured.given;
 
 
 @SpringBootTest
-class DemoApplicationTests {
+public class StubsApplicationTests {
 
     private static WireMockServer wireMockServer = new WireMockServer(WireMockConfiguration.options().port(5050));
 
-
+/**
+ * В методе setUpMockServer() происходит конфигурирование заглушек для API
+ */
     @BeforeAll
     public static void setUpMockServer(){
         wireMockServer.start();
         WireMock.configureFor("localhost", 5050);
+
         WireMock.stubFor(WireMock.get(WireMock.urlEqualTo("/api/users/2"))
                 .willReturn(WireMock.aResponse()
                         .withStatus(200)
@@ -40,11 +43,42 @@ class DemoApplicationTests {
                                 "    }\n" +
                                 "}")));
 
+        WireMock.stubFor(WireMock.get(WireMock.urlEqualTo("/api/unknown/2"))
+                .willReturn(WireMock.aResponse()
+                        .withStatus(200)
+                        .withBody("{\n" +
+                                "    \"data\": {\n" +
+                                "        \"id\": 2,\n" +
+                                "        \"name\": \"fuchsia rose\",\n" +
+                                "        \"year\": 2001,\n" +
+                                "        \"color\": \"#C74375\",\n" +
+                                "        \"pantone_value\": \"17-2031\"\n" +
+                                "    },\n" +
+                                "    \"support\": {\n" +
+                                "        \"url\": \"https://reqres.in/#support-heading\",\n" +
+                                "        \"text\": \"To keep ReqRes free, contributions towards server costs are appreciated!\"\n" +
+                                "    }\n" +
+                                "}")));
+
+        WireMock.stubFor(WireMock.post(WireMock.urlEqualTo("/api/users"))
+                .willReturn(WireMock.aResponse()
+                        .withStatus(200)
+                        .withBody("{\n" +
+                                "    \"name\": \"morpheus\",\n" +
+                                "    \"job\": \"leader\",\n" +
+                                "    \"id\": \"349\",\n" +
+                                "    \"createdAt\": \"2021-07-04T16:08:45.876Z\"\n" +
+                                "}")));
     }
 
-    @Test
-    void contextLoads() {
 
+/**
+ * В методе checkingSingleUser() происходит проверка существующего пользователя
+ * Шаг 1: Отправить запрос на поиск пользователя
+ *      Ожидаемый результат: Возвращается успешный ответ с информацией о пользователе
+ */
+    @Test
+    void checkingSingleUser() {
         Response response = given()
                 .contentType(ContentType.JSON)
                 .when()
@@ -53,15 +87,59 @@ class DemoApplicationTests {
                 .extract().response();
 
         Assertions.assertEquals(200, response.statusCode());
-        System.out.println(response.getBody().prettyPrint());
         Assertions.assertEquals("Janet", response.jsonPath().getString("data.first_name"));
         Assertions.assertEquals("Weaver", response.jsonPath().getString("data.last_name"));
-
+        System.out.println(response.getBody().prettyPrint());
     }
 
+
+/**
+ * В методе checkingSingleResource() происходит проверка существующего ресурса
+ * Шаг 1: Отправить запрос на поиск ресурса
+ *      Ожидаемый результат: Возвращается успешный ответ с информацией о ресурсе
+ */
+    @Test
+    void checkingSingleResource() {
+        Response response = given()
+                .contentType(ContentType.JSON)
+                .when()
+                .get("http://localhost:5050/api/unknown/2")
+                .then()
+                .extract().response();
+
+        Assertions.assertEquals(200, response.statusCode());
+        Assertions.assertNotNull(response.jsonPath().getString("data.id"));
+        Assertions.assertNotNull(response.jsonPath().getString("data.name"));
+        System.out.println(response.getBody().prettyPrint());
+    }
+
+
+/**
+ * В методе checkingUserCreation() происходит проверка создания пользователя
+ * Шаг 1: Отправить запрос на создание нового пользователя
+ *      Ожидаемый результат: Возвращается ответ об успешном создании нового пользователя
+ */
+    @Test
+    void checkingCreateUser() {
+        Response response = given()
+                .contentType(ContentType.JSON)
+                .when()
+                .post("http://localhost:5050/api/users")
+                .then()
+                .extract().response();
+
+        Assertions.assertEquals(200, response.statusCode());
+        Assertions.assertNotNull(response.jsonPath().getString("id"));
+        Assertions.assertNotNull(response.jsonPath().getString("name"));
+        System.out.println(response.getBody().prettyPrint());
+    }
+
+
+/**
+ * В методе tearDownMockServer() происхдит остановка wireMockServer
+ */
     @AfterAll
     public static void tearDownMockServer(){
         wireMockServer.stop();
     }
-
 }
